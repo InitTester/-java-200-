6738187a1cb27ad2fbc816e2acb58211eebb4a5c
InitTester <Initsave@gmail.com>
2024-04-07 11:48:17 +0900

    20240407 - test first  commit

---
+++ java200/src/java200/Q004.java
@@ -0,0 +1,15 @@
+package java200;
+
+public class Q004 {
+	/* È­¾¾ -> ¼·¾¾
+	 * 5/9*(fahrenheit-32)
+	 * 
+	 * */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		//
+	}
+	
+//	public 
+
+}
