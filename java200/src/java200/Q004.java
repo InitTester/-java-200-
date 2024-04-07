7d5b69c0e47fb9c2e954f46fb8ffbf97714f5fc9
InitTester <Initsave@gmail.com>
2024-04-07 11:50:32 +0900

    인코딩 수정 commit

--- java200/src/java200/Q004.java
+++ java200/src/java200/Q004.java
@@ -1,9 +1,9 @@
 package java200;
 
 public class Q004 {
-	/* È­¾¾ -> ¼·¾¾
+	/* í™”ì”¨ -> ì„­ì”¨
 	 * 5/9*(fahrenheit-32)
-	 * 
+	 *  ë³€ê²½
 	 * */
 	public static void main(String[] args) {
 		System.out.println("hello world");
